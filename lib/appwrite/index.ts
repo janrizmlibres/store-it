44dcae1642d9f67472f4ab0df6300f857da20c03
Janriz Libres <libres.janriz@gmail.com>
2024-11-17 12:23:38 +0800

    setup appwrite & create account

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { Account, Client, Databases, Storage, Avatars } from "node-appwrite";
+
+import { appwriteConfig } from "./config";
+
+// Creates client instance tied to a user session
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) throw new Error("No session");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+// Creates client instance with admin level permissions
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Whenever passing large payloads through server actions,
+// it is recommended to stringify and then parse the value
+export const parseStringify = (value: unknown) => {
+  return JSON.parse(JSON.stringify(value));
+};
